daa2bdf4992359b44035e20e2b8121db91d5b4c4
JuanSCadena <js.cadena@outlook.com>
2026-02-18 21:06:18 -0500

    mejora bugs

--- components/sections/Manifesto.tsx
+++ components/sections/Manifesto.tsx
@@ -8,7 +8,7 @@ export default function Manifesto() {
         visible: {
             opacity: 1,
             y: 0,
-            transition: { duration: 0.8, ease: "easeOut" }
+            transition: { duration: 0.8, ease: "easeOut" as const }
         }
     };
 
